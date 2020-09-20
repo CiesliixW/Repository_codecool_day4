0d81a8e36a32f7b48963e060e68335879eecf1d1
Wojciech Cieslik <cieslikw.96@gmail.com>
2020-09-20 10:01:08 +0200

    elo 2

---
+++ parser.py
@@ -0,0 +1,20 @@
+import sys
+
+def get_filename():
+    if len(sys.argv) >= 2:
+        filename = sys.argv[1]
+        return filename
+    else:
+        print("You should run this program by calling 333 python parser.py filename")
+        return ""
+
+def main(): 
+    filename = get_filename()
+
+    if len(filename) == 0:
+        return
+
+    print(f"File to parse: {filename}")
+
+if __name__ == "__main__":
+    main()
