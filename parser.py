9e26f522a8260a83171a5b9b0e4bcba1ccc5a27d
Wojciech Cieslik <cieslikw.96@gmail.com>
2020-09-20 15:04:39 +0200

    parser final

--- parser.py
+++ parser.py
@@ -1,57 +1,78 @@
+#
+#   program usage: python parser.py data\input.txt my_output.txt
+#
+
+###aaa
+
+
 import sys
 
 NAME_INDEX = 0
 
-def get_filename():
-    if len(sys.argv) >= 2:
-        filename = sys.argv[1]
+
+def get_filename(my_args):
+    if len(my_args) >= 3:
+        filename = my_args[1]
         return filename
     else:
-        print("You should run this program by calling python parser.py filename")
+        print("[ WARNING ] You should run this program by calling: python parser.py filename")
+        return ""
+
+
+def get_reportname():
+    if len(sys.argv) >= 3: 
+        report_name = sys.argv[2]
+        return report_name
+    else: 
+        print("[ WARNING ] You should run this program by calling: python parser.py filename")
         return ""
 
 def read_from_file_to_list(filename):
-    output =[]
-    with open(filename, "r") as file_to_read :
-        for line in file_to_read.readlines():
-            row  = line.replace("\n", "").split(" ")
-            output.append(row)
+    output = []
 
+    with open(filename,"r") as file_to_read:
+        for line in file_to_read.readlines():
+            row = line.replace("\n","").split(" ")
+            output.append(row)
+    
     return output
 
 def get_names(data_list):
-    name = []
+    names = []
 
     for line in data_list:
-        name.append(line[NAME_INDEX])
-    
-    return name
+        names.append(line[NAME_INDEX])
 
-def save_report_to_file(report, filename):
-    with open("output.txt", "w+") as file_to_read:
-        for report_data in report:
-            file_to_read.write(report_data)
+    return ", ".join(names)
 
 def generate_report(data_list):
     output = []
 
+    # inforamcja o pliku wejscowym
+    # inforamcja o pliku wyjscowym
     output.append(f"Ilosc danych {len(data_list)}")
-    output.append(f"Imiona{get_names(data_list)}")
+    output.append(f"Imiona: {get_names(data_list)}")
 
     return output
 
-def main(): 
-    filename = get_filename()
+def save_report_to_file(report, filename):
+    filename = get_reportname()
+    with open(filename, "w+") as file_to_read:
+        for report_data in report:
+            file_to_read.write(report_data + "\n")
 
+def main():
+    my_args = list(sys.argv)
+    filename = get_filename(my_args)
+    reportname = get_reportname()
     if len(filename) == 0:
         return
-
+    
     print(f"File to parse: {filename}")
     data_list = read_from_file_to_list(filename)
-    print(data_list)
     report = generate_report(data_list)
-    print(report)
-    save_report_to_file(report, "output.txt")
+    save_report_to_file(report, reportname)
+
 
 if __name__ == "__main__":
     main()
